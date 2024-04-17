256b6fcf25fd68d0d56f68e626e9275ede8c1f8a
Hiếu Ngân <ngan.le@kyanon.digital>
2024-04-17 23:18:25 +0700

    init project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-shadow': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    'react-native/no-inline-styles': 0,
+    'react-hooks/exhaustive-deps': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        'no-inline-styles': false,
+      },
+    ],
+  },
 };
